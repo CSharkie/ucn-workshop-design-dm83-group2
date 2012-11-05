f64289935306174003b1c0cca3320f9965329f42
cryszone@gmail.com <cryszone@gmail.com@150f95ca-57a2-a543-0132-6f4c4a137ce6>
2012-11-05 09:36:15 +0000

    first commit; Added the initial code

---
+++ trunk/Model/Person.java
@@ -0,0 +1,58 @@
+package Model;
+import java.util.HashSet;
+
+/**
+ * Write a description of class Person here.
+ * 
+ * @author 
+ * @version 
+ */
+public class Person
+{
+    private int id;
+    private String name;
+    private String address;
+    private int postalCode;
+    private String city;
+    private String phone;
+
+    /**
+     * Constructor for objects of class Person
+     */
+    public Person()
+    {
+        
+    }
+    
+    public int getId()
+    {
+        return id;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public String getAddress()
+    {
+        return address;
+    }
+    
+    public int getPostalCode()
+    {
+        return postalCode;
+    }
+    
+    public String getCity()
+    {
+        return city;
+    }
+    
+    public String getPhone()
+    {
+        return phone;
+    }
+
+    
+}

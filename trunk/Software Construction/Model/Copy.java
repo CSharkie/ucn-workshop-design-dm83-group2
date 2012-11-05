6ddab994012b18c65c9eb79eb3f2d69c2750150e
cryszone@gmail.com <cryszone@gmail.com@150f95ca-57a2-a543-0132-6f4c4a137ce6>
2012-11-05 14:40:40 +0000

    Prepared each class and added the update method for the AddressBook.

--- trunk/Software Construction/Model/Copy.java
+++ trunk/Software Construction/Model/Copy.java
@@ -9,27 +9,14 @@ package Model;
  */
 public class Copy
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private DVD dvd;
 
     /**
      * Constructor for objects of class Copy
      */
     public Copy()
     {
-        // initialise instance variables
-        x = 0;
+        
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
